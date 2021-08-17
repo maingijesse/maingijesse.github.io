5423729deb606fe4c41c62e763185d22e0973c9c
Jesse Maingi <maingijesse@gmail.com>
2021-08-17 19:17:45 +0300

    fixed form error

--- _nuxt/static/1629212912/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629212912",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"74d0372c"}}}}(null,"\u002F"));
